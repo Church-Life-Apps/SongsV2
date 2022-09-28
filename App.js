31db830fe87e4481248285804193018b6c554573
Brandon Xia <brandonxia01@gmail.com>
2022-09-27 22:50:47 -0400

    stuff to home page and add icons

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
+const initialNum = Math.floor(Math.random() * 533) + 1;
+const link = "https://raw.githubusercontent.com/Church-Life-Apps/Resources/master/resources/images/shl/SHL_";
+const suffix = ".png";
 
 export default function App() {
+  const [num, setNum] = React.useState(initialNum);
+
+  let refreshNum = async () => {
+    setNum(Math.floor(Math.random() * 533) + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Hymnal App V2! Hi!</Text>
+      <Image source={{ uri: link + String(num).padStart(3, "0") + suffix }} style={styles.image}></Image>
+      <TouchableOpacity onPress={refreshNum} style={styles.button}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,11 +27,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   welcomeText: {
-    fontSize: 50
-  }
+    fontSize: 50,
+  },
+  image: {
+    width: 600,
+    height: 600,
+    resizeMode: "contain",
+  },
+  button: {
+    backgroundColor: "cyan",
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
 });
