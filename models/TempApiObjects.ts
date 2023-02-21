5f323f25fe80ef2b4ba47e1a3ba2bfd93b6f4288
daniel-727 <zaoster777@gmail.com>
2023-02-21 10:55:16 -0600

    updated TempApiObjects

--- models/TempApiObjects.ts
+++ models/TempApiObjects.ts
@@ -8,64 +8,69 @@ import {
 } from "./SongsApiModels";
 
 // hard coded Data object for individual songbook in Songbooks List API
-const SHL: Songbook = {
+export const SHL: Songbook = {
   id: "shl",
   fullName: "Songs and Hymns of Life",
   staticMetadataLink:
     "https://github.com/Church-Life-Apps/Resources/blob/master/resources/metadata/shl.json",
-  imageUrl: "not sure where to find the URL right now",
+  imageUrl:
+    "https://i0.wp.com/asweetsavor.org/wp-content/uploads/SHL-on-Piano-e1602105678283.jpg?fit=882%2C686&ssl=1",
 };
 
 // hard coded Data object for individual song in songlist
-const Glory: Song = {
+export const TEST_SONG: Song = {
   id: uuidv4,
   songbookId: "shl",
   number: 1,
-  title: "Glory Be To God The Father",
-  author: "Horatius Bonar",
-  music: "Henry Thomas Smart (1813-1879)",
-  presentationOrder: "v1 v2 v3 v4",
+  title: "Great Is Thy Faithfulness",
+  author: "Thomas O. Chrisholm",
+  music: "William Marion Runyan (1870-1957)",
+  presentationOrder: "v1 c1 v2 c1 v3 c1",
   imageUrl:
-    "https://raw.githubusercontent.com/Church-Life-Apps/Resources/master/resources/images/shl/SHL_001.png",
+    "https://raw.githubusercontent.com/Church-Life-Apps/Resources/master/resources/images/shl/SHL_010.png",
 };
 
 // hard coded Data object for Lyrics
-const Glory_V1: Lyric = {
+export const TEST_LYRIC_V1: Lyric = {
   songId: uuidv4,
   lyricType: LyricType.LYRIC_TYPE_VERSE,
   verseNumber: 1,
-  lyrics: `"Glory be to God the Father,",
-   "Glory be to God the Son,",
-   "Glory be to God the Spirit,",
-   "Great Jehovah, Three in One!",
-   "As it was, is now, and shall be",
-   "While the endless ages run."`,
+  lyrics: `"Great is Thy faithfulness," O God my Father,
+  There is no shadow of turning with Thee;
+  Thou changest not, Thy compassions, they fail not;
+  As Thou hast been Thou forever wilt be.`,
 };
-const Glory_V2: Lyric = {
+export const TEST_LYRIC_V2: Lyric = {
   songId: uuidv4,
   lyricType: LyricType.LYRIC_TYPE_VERSE,
   verseNumber: 2,
-  lyrics: `"Glory be to Him Who loved us,",
-  "Washed us from each spot and stain!",
-  "Glory be to Him Who bought us,",
-  "Made us kings with Him to reign!",
-  "Glory, glory, glory, glory",
-  "To the Lamb that once was slain!"`,
+  lyrics: `Summer and winter, and springtime and harvest.
+  Sun, moon and stars in their courses above,
+  Join with all nature in manifold witness,
+  To Thy great faithfulness, mercy and love.`,
 };
-const Glory_V3: Lyric = {
+export const TEST_LYRIC_V3: Lyric = {
   songId: uuidv4,
   lyricType: LyricType.LYRIC_TYPE_VERSE,
   verseNumber: 3,
-  lyrics: `"Glory to the King of angels,",
-  "Glory to the Church's King,",
-  "Glory to the King of nations!",
-  "Heav'n and earth, your praises bring;",
-  "Glory, glory, glory, glory",
-  "To the King of glory bring!"`,
+  lyrics: `Pardon for sin and a peace that endureth.
+  Thy own dear presence to cheer and to guide;
+  Strength for today and bright hope for tomorrow,
+  Blessings all mine, with ten thousand beside!`,
+};
+export const TEST_CHORUS_C1: Lyric = {
+  songId: uuidv4,
+  lyricType: LyricType.LYRIC_TYPE_CHORUS,
+  verseNumber: 1,
+  lyrics: `"Great is Thy faithfulness!"
+  "Great is Thy faithfulness!"
+  Morning by morning new mercies I see;
+  All I have needed Thy hand hath provided \u2014
+  "Great is Thy faithfulness," Lord, unto me!`,
 };
 
 // hard coded Data object for Songs with Lyrics
-const Glory_with_Lyrics: SongWithLyrics = {
-  song: Glory,
-  lyrics: [Glory_V1, Glory_V2, Glory_V3],
+export const TEST_SONG_WITH_LYRIC: SongWithLyrics = {
+  song: TEST_SONG,
+  lyrics: [TEST_LYRIC_V1, TEST_LYRIC_V2, TEST_LYRIC_V3, TEST_CHORUS_C1],
 };
