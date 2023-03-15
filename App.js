da8bf1aa7ca6b6777a31c85248a28154b2700937
calebziamba <cmz4by@virginia.edu>
2023-03-14 22:40:10 -0400

    support deep linking

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import SongListScreen from "./screens/SongListScreen";
+import * as Linking from "expo-linking";
 
 const Stack = createNativeStackNavigator();
+const prefix = Linking.createURL("/");
+const config = {
+  screens: {
+    Home: "",
+    Songlist: ":songbookId"
+  }
+}
+const linking = {
+  prefixes: [prefix],
+  config,
+};
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}>
-        </Stack.Screen>
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
         <Stack.Screen
           name="Songlist"
           component={SongListScreen}
-          options={({ route }) => ({ title: route.params.title })}>
-        </Stack.Screen>
+          options={({ route }) => ({ title: route.params.title })}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
