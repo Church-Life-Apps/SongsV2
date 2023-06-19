4435871682da5869442c237d03e65af1e7fde9a1
Daniel Li <zaoster777@gmail.com>
2023-06-19 18:14:49 -0500

    move AudioPlayerComponent to SongScreen

--- components/AudioPlayerComponent.tsx
+++ components/AudioPlayerComponent.tsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button } from "react-native";
+import { ActivityIndicator, View, Text, Button } from "react-native";
 import { Audio } from "expo-av";
 
-const AudioPlayerComponent = () => {
+// Define the component props
+interface AudioPlayerComponentProps {
+  audioUrl: string;
+}
+
+const AudioPlayerComponent: React.FC<AudioPlayerComponentProps> = ({ audioUrl }) => {
   const [loaded, setLoaded] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [audio, setAudio] = useState(null);
 
   const loadAudio = async () => {
     try {
-      // Creating and Loading our music object and making sure it downloads first
-      const audio = await Audio.Sound.createAsync(require("../assets/sound.mp3"), {}, null, true);
+      // Creating and Loading our music object
+      /* const audio = await Audio.Sound.createAsync(require("../assets/sound.mp3"), {}, null, true); */
+      const audio = await Audio.Sound.createAsync({ uri: audioUrl }, {}, null);
       setLoaded(audio.sound._loaded);
       setAudio(audio);
     } catch (err) {
@@ -25,7 +31,7 @@ const AudioPlayerComponent = () => {
   const playAudio = async () => {
     try {
       const status = await audio.sound.playAsync();
-      setIsPlaying(status.isPlaying);
+      setIsPlaying(true);
     } catch (err) {
       console.log(err);
     }
@@ -34,7 +40,7 @@ const AudioPlayerComponent = () => {
   const pauseAudio = async () => {
     try {
       const status = await audio.sound.pauseAsync();
-      setIsPlaying(status.isPlaying);
+      setIsPlaying(false);
     } catch (err) {
       console.log(err);
     }
@@ -49,7 +55,7 @@ const AudioPlayerComponent = () => {
           <Button title="Play Sound" onPress={playAudio} />
         )
       ) : (
-        <Text>Loading... </Text>
+        <ActivityIndicator />
       )}
     </View>
   );
