3b0461c5fc0a5cec131d9615f51dbc21a81d14c5
Caleb Ziamba <cmz4by@virginia.edu>
2023-12-14 21:24:03 -0500

    missed file

---
+++ components/SongItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { globalStyles, listStyles } from "../styles/GlobalStyles";
+import { Song } from "../models/SongsApiModels";
+
+export const SongItem = ({ song, onPress }: { song: Song; onPress: any; }) => (
+  <TouchableOpacity onPress={onPress} style={[listStyles.item]}>
+    <Text style={[listStyles.number]}>{song.number}</Text>
+    <View style={{flex: 1}}>
+      <Text style={[listStyles.title]}>
+        {song.title}
+      </Text>
+      <Text style={[listStyles.subTitle]}>{song.author}</Text>
+    </View>
+  </TouchableOpacity>
+);
