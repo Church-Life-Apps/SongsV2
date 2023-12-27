4e738e8d51c4a39ca86b0caaf8f05d830a2417aa
Caleb Ziamba <cziamba@gmail.com>
2023-12-27 18:15:57 -0500

    Tab styling use nativewind

--- components/Tab/Tab.tsx
+++ components/Tab/Tab.tsx
@@ -11,26 +11,13 @@ export const Tab = ({ active, title, onPress = () => {}, ...rest }: TabProps) =>
   return (
     <Pressable
       onPress={() => onPress()}
-      style={[styles.buttonStyle, { backgroundColor: active ? "#5cc0d6" : "white" }]}
+      className="rounded mx-0.5 bg-none bg-white hover:bg-gray-200"
+      style={[{ backgroundColor: active ? "#5cc0d6" : undefined }]}
       {...rest}
     >
-      <Text style={[styles.titleStyle, { color: active ? "white" : "#5cc0d6" }]}>{title}</Text>
+      <Text className="px-4 py-2" style={[{ color: active ? "white" : "#5cc0d6" }]}>
+        {title}
+      </Text>
     </Pressable>
   );
 };
-
-const styles = StyleSheet.create({
-  buttonStyle: {
-    borderRadius: 4,
-    backgroundColor: "transparent",
-    marginHorizontal: 2,
-  },
-  titleStyle: {
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-  },
-  containerStyle: {
-    flex: 1,
-    borderRadius: 0,
-  },
-});
