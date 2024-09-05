3d35ff599affe33147ec08f9dc481339add0f1d9
Caleb Ziamba <cmz4by@virginia.edu>
2024-09-04 22:48:06 -0400

    eslint ignore top level config files for cjs or mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
+  // ignore top level config files, so they can be module or commonjs
+  {ignores: ["*.cjs", "*.mjs", "/dist/*"]},
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
