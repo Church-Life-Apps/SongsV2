26fb335fd823f5808aabd646aa304c1fb2c9d9e5
Caleb Ziamba <cmz4by@virginia.edu>
2024-10-26 16:10:51 -0400

    download music button

--- components/Button.tsx
+++ components/Button.tsx
@@ -19,6 +19,7 @@ interface ButtonProps {
   title: string;
   variant: Variant;
   className?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   icon?: any;
   disabled?: boolean;
 }
