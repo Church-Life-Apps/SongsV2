f891d0f024ec0b090380fffd084b762df5ab878d
Caleb Ziamba <cmz4by@virginia.edu>
2024-10-25 22:39:50 -0400

    better responsiveness on books page

--- components/SongbookList.tsx
+++ components/SongbookList.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Text, Image, TouchableOpacity, View, useWindowDimensions } from "react-native";
 import { Songbook } from "../models/SongsApiModels";
 import { FlatGrid } from "react-native-super-grid";
@@ -14,13 +15,14 @@ const SongbookList = ({ songbooks, onPress }: { songbooks: Songbook[]; onPress: 
   const bookWidth =
     windowWidth > 700
       ? Math.min(windowWidth / 3 - 25, 325)
-      : windowWidth > 350
+      : windowWidth > 400
         ? Math.floor(windowWidth / 2) - 30
-        : Math.min(windowWidth - 30, 300);
+        : Math.min(windowWidth - 40, 380);
 
   return (
-    <View className="w-screen h-screen overflow-y-auto pb-20 mt-8 md:mt-0">
+    <View className="w-screen h-screen overflow-y-auto pb-20 mt-8 md:mt-0 align-center">
       <FlatGrid
+        itemContainerStyle={{alignSelf: "flex-start"}}
         itemDimension={bookWidth}
         data={songbooks.map((book) => ({
           data: book,
@@ -40,8 +42,8 @@ const SongbookList = ({ songbooks, onPress }: { songbooks: Songbook[]; onPress: 
               resizeMode="cover"
             />
             <Text
-              style={{ fontWeight: 400, fontSize: 24, fontFamily: "serif" }}
-              className="mt-2 text-center whitespace-nowrap"
+              style={{ fontWeight: 400, fontSize: 24 }}
+              className="mt-2 text-center whitespace-wrap"
             >
               {item.data.fullName}
             </Text>
