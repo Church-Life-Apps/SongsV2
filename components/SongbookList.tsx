5fb65c953409b52eb0f74d1604f52626dc7161d7
Caleb Ziamba <cmz4by@virginia.edu>
2024-10-26 12:17:38 -0400

    fix songbook colors

--- components/SongbookList.tsx
+++ components/SongbookList.tsx
@@ -43,7 +43,7 @@ const SongbookList = ({ songbooks, onPress }: { songbooks: Songbook[]; onPress: 
             />
             <Text
               style={{ fontWeight: 400, fontSize: 24 }}
-              className="mt-2 text-center whitespace-wrap"
+              className="mt-2 text-center whitespace-wrap text-typography-light dark:text-typography-dark"
             >
               {item.data.fullName}
             </Text>
