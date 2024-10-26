09e215b63c64c83f67f496be6deb64333809ae10
Caleb Ziamba <cmz4by@virginia.edu>
2024-10-26 12:07:29 -0400

    keep grid height within window

--- components/SongbookList.tsx
+++ components/SongbookList.tsx
@@ -20,7 +20,7 @@ const SongbookList = ({ songbooks, onPress }: { songbooks: Songbook[]; onPress: 
         : Math.min(windowWidth - 40, 380);
 
   return (
-    <View className="w-screen h-screen overflow-y-auto pb-20 mt-2 md:mt-0 align-center">
+    <View className="w-screen pb-20 mt-2 md:mt-0 align-center">
       <FlatGrid
         itemContainerStyle={{alignSelf: "flex-start"}}
         itemDimension={bookWidth}
