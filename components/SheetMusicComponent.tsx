d5e4a74f7dfd93607b16c7a5e9b3a71c61c39ed4
calebziamba <cziamba@gmail.com>
2024-10-28 22:05:44 -0400

    download music button (#53)

--- components/SheetMusicComponent.tsx
+++ components/SheetMusicComponent.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import { View } from "react-native";
 import ImageModal from "react-native-image-modal";
+import Button from "./Button";
 
-// define the component props
 interface SheetMusicComponentProps {
   imageUrl: string;
 }
 
-// used Dimensions to get specific device window width and height, we may want to move this to our screens.
-
 const SheetMusicComponent: React.FC<SheetMusicComponentProps> = ({ imageUrl }) => {
+  const handleDownload = async () => {
+    // TODO : ensure this works with mobile platforms, should we decide to expand from web-only
+    fetch(imageUrl as string)
+      .then(response => response.blob())
+      .then(blob => {
+        const blobUrl = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.download = imageUrl.replace(/^.*[\\/]/, '');
+        a.href = blobUrl;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+      })
+  }
+
   return (
     <View style={{ flex: 1, alignSelf: "center" }}>
       <ImageModal source={{ uri: imageUrl }} style={{ width: 320, height: 500 }} resizeMode="contain" />
+      <Button
+        onPress={handleDownload}
+        variant="outlineSlate"
+        title="Download "
+        icon="download"
+        className="mx-3"
+      ></Button>
     </View>
   );
 };
