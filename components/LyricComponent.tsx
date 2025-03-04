4ecc928f178a00b8c86b08f2ce1b6dbb6057cb3b
calebziamba <cmz4by@virginia.edu>
2025-03-03 23:29:26 -0500

    center lyric title

--- components/LyricComponent.tsx
+++ components/LyricComponent.tsx
@@ -17,7 +17,7 @@ const LyricComponent: React.FC<LyricComponentProps> = ({ songData, removeDuplica
   const content = (
     <>
       <Text className="ms-auto text-xl font-semibold text-zinc-500 dark:text-zinc-400">{songData.number}</Text>
-      <Text className="text-4xl font-bold my-2 text-zinc-700 dark:text-gray-200">{songData.title}</Text>
+      <Text className="text-center text-4xl font-bold my-2 text-zinc-700 dark:text-gray-200">{songData.title}</Text>
       <View className="mt-2 mb-10">
         {lyricBlocks.map((lyric, lyricIndex) => (
           <View key={`View ${lyricIndex}`} className="my-2">
