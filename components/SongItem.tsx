214cf07de046ba9e9071dded0d3915299d942ba3
calebziamba <cmz4by@virginia.edu>
2025-06-05 23:05:56 -0400

    bigger still

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -9,10 +9,10 @@ const itemTextClasses = "text-typography-light dark:text-typography-dark hover:t
 
 export const SongItem = ({ song, onPress }: { song: Song; onPress: () => void }) => (
   <TouchableOpacity onPress={onPress} className={itemStructuralClasses + itemBackgroundClasses + itemBorderClasses + itemTextClasses}>
-    <View className="flex-1 me-3">
+    <View className="flex-1 me-5">
       <Text className="text-lg leading-[1.1] font-semibold max-w-full text-inherit">{song.title}</Text>
       <Text className="text-sm text-muted-light dark:text-muted-dark">{song.author}</Text>
     </View>
-    <Text className="text-3xl text-inherit">{song.number}</Text>
+    <Text className="text-4xl text-inherit leading-none">{song.number}</Text>
   </TouchableOpacity>
 );
