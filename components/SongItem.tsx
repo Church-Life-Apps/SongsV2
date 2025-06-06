bbabf04d90b043a264c284e9c71d96cbf36ef972
calebziamba <cmz4by@virginia.edu>
2025-06-05 22:59:47 -0400

    back to big, centered numbers

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { Song } from "../models/SongsApiModels";
 
-const itemStructuralClasses = "flex-row items-start py-3 px-3 ";
+const itemStructuralClasses = "flex-row items-center py-3 px-3 ";
 const itemBackgroundClasses = "bg-background-light dark:bg-background-dark hover:bg-gray-200 active:bg-gray-300 hover:dark:bg-neutral-900 active:dark:bg-neutral-950 ";
 const itemBorderClasses = "border-slate-700 dark:border-neutral-600 ";
 const itemTextClasses = "text-typography-light dark:text-typography-dark hover:text-gray-950 "
@@ -10,9 +10,9 @@ const itemTextClasses = "text-typography-light dark:text-typography-dark hover:t
 export const SongItem = ({ song, onPress }: { song: Song; onPress: () => void }) => (
   <TouchableOpacity onPress={onPress} className={itemStructuralClasses + itemBackgroundClasses + itemBorderClasses + itemTextClasses}>
     <View className="flex-1 me-3">
-      <Text className="text-lg leading-1 font-semibold max-w-full text-inherit">{song.title}</Text>
+      <Text className="text-lg leading-[1.1] font-semibold max-w-full text-inherit">{song.title}</Text>
       <Text className="text-sm text-muted-light dark:text-muted-dark">{song.author}</Text>
     </View>
-    <Text className="text-xl text-inherit text-muted-light dark:text-muted-dark">{song.number}</Text>
+    <Text className="text-3xl text-inherit">{song.number}</Text>
   </TouchableOpacity>
 );
