add79fb3e40d689245e76c558e850039ac7b9f24
calebziamba <cmz4by@virginia.edu>
2025-06-05 22:04:53 -0400

    modernize songbook page

--- components/BookItem.tsx
+++ components/BookItem.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, Image, TouchableOpacity } from "react-native";
 import { Songbook } from "../models/SongsApiModels";
 
+const images = {
+  SHL: require("../assets/images/book_covers/SHL.jpg"),
+  SFOG: require("../assets/images/book_covers/SFOG.png"),
+  GENERIC: require("../assets/images/book_covers/generic.jpg"),
+  // Add more book cover images here
+};
+
+type ImageKeys = keyof typeof images; // "SHL" | "SFOG" | "GENERIC"
+
+const itemBackgroundClasses = "bg-background-light dark:bg-background-dark hover:bg-gray-200 active:bg-gray-300 hover:dark:bg-neutral-900 active:dark:bg-neutral-950 ";
+
+
 export const BookItem = ({ songbook, onPress }: { songbook: Songbook; onPress: any }) => (
-  <TouchableOpacity className="flex-row items-center py-4 px-3 my-1 mx-4 rounded border border-s-8" onPress={onPress}>
-    <Text className="text-base font-bold max-w-full">{songbook.fullName}</Text>
+  <TouchableOpacity className={"flex-row items-center py-1 px-4 my-1 border-slate-700 dark:border-neutral-600 " + itemBackgroundClasses} onPress={onPress}>
+    <Image
+      source={images[songbook.id.toUpperCase() as ImageKeys] ?? images["GENERIC"]}
+      alt="Songbook Cover"
+      style={{ width: 64, height: 64 }}
+      className="rounded object-contain me-4"
+      resizeMode="cover"
+    />
+    <Text className="text-lg text-typography-light dark:text-typography-dark hover:text-gray-950 max-w-full">{songbook.fullName}</Text>
   </TouchableOpacity>
 );
--- components/SongbookList.tsx
+++ components/SongbookList.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Text, Image, TouchableOpacity, View, useWindowDimensions } from "react-native";
+import { Text, Image, TouchableOpacity, View, useWindowDimensions, FlatList } from "react-native";
 import { Songbook } from "../models/SongsApiModels";
 import { FlatGrid } from "react-native-super-grid";
+import { BookItem } from "./BookItem";
 
 const SongbookList = ({ songbooks, onPress }: { songbooks: Songbook[]; onPress: (item: Songbook) => void }) => {
   const { width: windowWidth } = useWindowDimensions();
@@ -21,36 +22,34 @@ const SongbookList = ({ songbooks, onPress }: { songbooks: Songbook[]; onPress: 
         : Math.min(windowWidth - 40, 380);
 
   return (
-    <View className="w-screen pb-20 mt-2 md:mt-0 items-center">
-      <FlatGrid
-        itemContainerStyle={{alignSelf: "flex-start"}}
-        itemDimension={bookWidth}
+      <FlatList
+        className="w-full pt-5"
+        contentContainerClassName="w-full max-w-5xl self-center"
         data={songbooks.map((book) => ({
           data: book,
         }))}
-        spacing={20}
         renderItem={({ item }) => (
-          <TouchableOpacity
-            style={{ maxWidth: bookWidth, maxHeight: bookWidth * 1.5 }}
-            className="cursor-pointer mb-3"
-            onPress={() => onPress(item.data)}
-          >
-            <Image
-              source={images[item.data.id.toUpperCase() as ImageKeys] ?? images["GENERIC"]}
-              alt="Songbook Cover"
-              style={{ maxWidth: bookWidth, maxHeight: bookWidth }}
-              className="border-2 border-gray-900 rounded-lg object-contain"
-              resizeMode="cover"
-            />
-            <Text
-              className="mt-2 text-center font-normal text-lg whitespace-wrap text-typography-light dark:text-typography-dark"
-            >
-              {item.data.fullName}
-            </Text>
-          </TouchableOpacity>
+          <BookItem songbook={item.data}  onPress={() => onPress(item.data)}></BookItem>
+          // <TouchableOpacity
+          //   style={{ maxWidth: bookWidth, maxHeight: bookWidth * 1.5 }}
+          //   className="cursor-pointer mb-3"
+          //   onPress={() => onPress(item.data)}
+          // >
+          //   <Image
+          //     source={images[item.data.id.toUpperCase() as ImageKeys] ?? images["GENERIC"]}
+          //     alt="Songbook Cover"
+          //     style={{ maxWidth: bookWidth, maxHeight: bookWidth }}
+          //     className="border-2 border-gray-900 rounded-lg object-contain"
+          //     resizeMode="cover"
+          //   />
+          //   <Text
+          //     className="mt-2 text-center font-normal text-lg whitespace-wrap text-typography-light dark:text-typography-dark"
+          //   >
+          //     {item.data.fullName}
+          //   </Text>
+          // </TouchableOpacity>
         )}
       />
-    </View>
   );
 };
 
