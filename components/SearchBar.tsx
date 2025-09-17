0e11d6f1cba6edb1bef62bd3c1699e1deb4dd432
calebziamba <cmz4by@virginia.edu>
2025-09-16 20:40:11 -0400

    Add button to clear search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
-import { TextInput, TextStyle } from "react-native";
-import { debounce } from "lodash";
-import React from "react";
+import { TextInput, TextStyle, TouchableOpacity, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { debounce } from "../utils/FunctionUtils";
 
 interface SearchBarProps {
   value?: string;
@@ -11,19 +12,43 @@ interface SearchBarProps {
   className?: string;
 }
 
-const defaultStyle: TextStyle = {
-  height: "auto",
-  width: "100%",
-  fontSize: 16,
-  borderColor: "darkgray",
-  borderBottomWidth: 2,
-  paddingHorizontal: 8,
-  paddingVertical: 4,
-};
-
 export const SearchBar = ({ onChange, placeholder, style, debounceWait = 500, className }: SearchBarProps) => {
   const debounced = debounce((value) => {
     onChange(value);
   }, debounceWait);
-  return <TextInput className={className} style={{ ...defaultStyle, ...style }} onChangeText={debounced} placeholder={placeholder} />;
+  const inputRef = useRef<TextInput>(null);
+  const [value, setValue] = useState<string>();
+
+  const clearValue = () => {
+    setValue("");
+    onChange("");
+    inputRef.current?.focus();
+  };
+
+  const clearIcon = (
+    <TouchableOpacity
+      onPress={clearValue}
+      className={"bg-transparent text-xl justify-center border-b-2 border-neutral-300"}
+    >
+      <Feather name={"x"} className={"text-typography-light dark:text-typography-dark opacity-50"} size={16} />
+    </TouchableOpacity>
+  );
+
+  return (
+    <View className="flex-row align-center w-full max-w-[1000px] px-3">
+      <TextInput
+        value={value}
+        inputMode="search"
+        className={className}
+        style={{ ...style }}
+        onChangeText={(nextValue) => {
+          setValue(nextValue);
+          debounced(nextValue);
+        }}
+        placeholder={placeholder}
+        ref={inputRef}
+      />
+      {value && clearIcon}
+    </View>
+  );
 };
---
+++ utils/FunctionUtils.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => void>(fn: T, delay: number): (...args: Parameters<T>) => void {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
